11fdb72fcb77b00eedd6826a63466e9abefa4b44
Thaise Dantas | Stone <tdantas@stone.com.br>
2017-05-29 14:33:56 -0300

    first commit

---
+++ CalculadoraTest/Controllers/CalculadoraController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CalculadoraTest.Controllers
+{
+    public class CalculadoraController : ApiController
+    {
+
+
+     
+      public string Getone()
+        {
+            return "xxxxx";
+
+        }
+
+        public string Get(string id)
+        {
+            id = "1";
+
+            return id;
+
+        }
+
+
+
+    }
+}
---
+++ Test/Class1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using CalculadoraTest.Controllers;
+using System.Web.Http;
+
+
+namespace Test
+{
+    public class RecursosTeste
+    {
+
+       public CalculadoraController CalculadoraTest;
+
+
+
+
+        public RecursosTeste()
+        {
+            CalculadoraController CalculadoraTest = new CalculadoraController();
+
+        }
+
+
+    }
+
+
+    public class Class1 : IClassFixture<RecursosTeste>
+    {
+        RecursosTeste recurso;
+
+        public Class1(RecursosTeste recurso)
+        {
+            this.recurso = recurso;
+
+        }
+
+        [Fact]
+        public void TestandoGet()
+        {
+            string retorno = recurso.CalculadoraTest.Getone();
+
+
+        }
+
+
+
+    }
+}
